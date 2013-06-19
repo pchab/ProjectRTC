178a388f3eabf22a7be90f1c8cc9f17ec237994d
Pierre Chabardes <pierre@chabardes.net>
2013-06-19 08:28:52 -0700

    remove useless route

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/streams', routes.streams);
-app.post('/rename', routes.rename);
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
